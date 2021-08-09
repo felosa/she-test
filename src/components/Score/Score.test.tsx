184f0576a76141a72777ddd4e8a4d7cf22bfe70d
felosaknight <flo@archerknight.com>
2021-08-09 12:24:39 +0100

    refactore test, add className to tests

--- src/components/Score/Score.test.tsx
+++ src/components/Score/Score.test.tsx
@@ -7,10 +7,10 @@ import { render, screen } from "./test.utils";
 import {
   ANSWERS_CORRECT_2,
   ANSWERS_CORRECT_8,
-  questions,
+  QUESTIONS,
 } from "./test.constants";
 
-test("Show 0 correct answers and 0 questions", async () => {
+test("Show when there are no questions and answers", async () => {
   render(<Score />, {
     trivia: {
       questions: [],
@@ -18,51 +18,43 @@ test("Show 0 correct answers and 0 questions", async () => {
     },
   });
 
-  const correctAnswersQuantity = screen.getByTestId(
-    CORRECT_ANSWERS_DATA_TEST_ID
-  ).textContent;
+  const correctAnswers = screen.getByTestId(CORRECT_ANSWERS_DATA_TEST_ID);
 
-  const totalQuestions = screen.getByTestId(TOTAL_QUESTIONS_DATA_TEST_ID)
-    .textContent;
-
-  expect(correctAnswersQuantity).toBe("0");
-  expect(totalQuestions).toBe("0");
+  const totalQuestions = screen.getByTestId(TOTAL_QUESTIONS_DATA_TEST_ID);
+  expect(correctAnswers.textContent).toBe("0");
+  expect(totalQuestions.textContent).toBe("0");
 });
 
-test("Show 8 correct answers and 10 questions", async () => {
+test("Show the correct class, number of answers and questions when the test is passed", async () => {
   render(<Score />, {
     trivia: {
-      questions: questions,
+      questions: QUESTIONS,
       answers: ANSWERS_CORRECT_8,
     },
   });
 
-  const correctAnswersQuantity = screen.getByTestId(
-    CORRECT_ANSWERS_DATA_TEST_ID
-  ).textContent;
+  const correctAnswers = screen.getByTestId(CORRECT_ANSWERS_DATA_TEST_ID);
 
-  const totalQuestions = screen.getByTestId(TOTAL_QUESTIONS_DATA_TEST_ID)
-    .textContent;
+  const totalQuestions = screen.getByTestId(TOTAL_QUESTIONS_DATA_TEST_ID);
 
-  expect(correctAnswersQuantity).toBe("8");
-  expect(totalQuestions).toBe("10");
+  expect(correctAnswers).toHaveClass("passed");
+  expect(correctAnswers.textContent).toBe("8");
+  expect(totalQuestions.textContent).toBe("10");
 });
 
-test("Show 2 correct answers and 10 questions", async () => {
+test("Show the correct class, number of answers and questions when the test is not passed", async () => {
   render(<Score />, {
     trivia: {
-      questions: questions,
+      questions: QUESTIONS,
       answers: ANSWERS_CORRECT_2,
     },
   });
 
-  const correctAnswersQuantity = screen.getByTestId(
-    CORRECT_ANSWERS_DATA_TEST_ID
-  ).textContent;
+  const correctAnswers = screen.getByTestId(CORRECT_ANSWERS_DATA_TEST_ID);
 
-  const totalQuestions = screen.getByTestId(TOTAL_QUESTIONS_DATA_TEST_ID)
-    .textContent;
+  const totalQuestions = screen.getByTestId(TOTAL_QUESTIONS_DATA_TEST_ID);
 
-  expect(correctAnswersQuantity).toBe("2");
-  expect(totalQuestions).toBe("10");
+  expect(correctAnswers).toHaveClass("suspended");
+  expect(correctAnswers.textContent).toBe("2");
+  expect(totalQuestions.textContent).toBe("10");
 });
--- src/components/Score/test.constants.ts
+++ src/components/Score/test.constants.ts
@@ -1,4 +1,4 @@
-export const questions = [
+export const QUESTIONS = [
   {
     category: "Science%3A%20Mathematics",
     type: "multiple",
